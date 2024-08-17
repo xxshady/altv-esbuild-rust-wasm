f73d3759ef5f8009b9a49fa5dabeb50befcdd7cf
xshady <fastik036@gmail.com>
2024-08-17 21:32:38 +0300

    describe lifetime of each base object type

--- example/rust-wasm/src/base_objects/base_object_type.rs
+++ example/rust-wasm/src/base_objects/base_object_type.rs
@@ -4,35 +4,38 @@ use serde_repr::{Deserialize_repr, Serialize_repr};
 #[derive(Debug, Serialize_repr, Deserialize_repr, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
 pub enum BaseObjectType {
-  PLAYER,
-  VEHICLE,
-  PED,
-  OBJECT,
-  BLIP,
-  WEBVIEW,
-  VOICE_CHANNEL,
-  COLSHAPE,
-  CHECKPOINT,
-  WEBSOCKET_CLIENT,
-  HTTP_CLIENT,
-  AUDIO,
+  PLAYER,           // GAME_ENTITY_CREATE -> GAME_ENTITY_DESTROY
+  VEHICLE,          // GAME_ENTITY_CREATE -> GAME_ENTITY_DESTROY
+  PED,              // GAME_ENTITY_CREATE -> GAME_ENTITY_DESTROY
+  OBJECT,           // GAME_ENTITY_CREATE -> GAME_ENTITY_DESTROY
+  BLIP,             // WORLD_OBJECT_STREAM_IN -> WORLD_OBJECT_STREAM_OUT
+  WEBVIEW,          // local
+  VOICE_CHANNEL,    // server only
+  COLSHAPE,         // local (serverside one doesn't exist on clientside)
+  CHECKPOINT,       // local (serverside one doesn't exist on clientside?)
+  WEBSOCKET_CLIENT, // local
+  HTTP_CLIENT,      // local
+  AUDIO,            // local
+
+  // local (TODO: what to do when these are destroyed by audio?)
   AUDIO_OUTPUT,
   AUDIO_OUTPUT_WORLD,
   AUDIO_OUTPUT_ATTACHED,
   AUDIO_OUTPUT_FRONTEND,
-  RML_ELEMENT,
-  RML_DOCUMENT,
-  LOCAL_PLAYER,
-  LOCAL_OBJECT,
-  VIRTUAL_ENTITY,
-  VIRTUAL_ENTITY_GROUP,
-  MARKER,
-  TEXT_LABEL,
-  LOCAL_PED,
-  LOCAL_VEHICLE,
-  AUDIO_FILTER,
-  CONNECTION_INFO,
-  CUSTOM_TEXTURE,
-  FONT,
+
+  RML_ELEMENT,          // .
+  RML_DOCUMENT,         // .
+  LOCAL_PLAYER,         // local and static lifetime, always exist
+  LOCAL_OBJECT,         // local
+  VIRTUAL_ENTITY,       // local and server: WORLD_OBJECT_STREAM_IN -> WORLD_OBJECT_STREAM_OUT
+  VIRTUAL_ENTITY_GROUP, // local (server one doesn't exist on clientside)
+  MARKER,               // local
+  TEXT_LABEL,           // local
+  LOCAL_PED,            // local
+  LOCAL_VEHICLE,        // local
+  AUDIO_FILTER,         // local
+  CONNECTION_INFO,      // server only
+  CUSTOM_TEXTURE,       // what is it?
+  FONT,                 // local
   SIZE,
 }
