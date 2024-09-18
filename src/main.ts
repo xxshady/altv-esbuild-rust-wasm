bf942cfe52b1ca3b693f5f33605ac71904c7a4e7
xshady <fastik036@gmail.com>
2024-09-19 00:25:56 +0300

    pack .wasm binary separately
    if its not my schizo alt:V don't like it when you throw a lot of shit into one string (or .js file, i dont know)
    
    in my case gameEntityCreate event was triggering two times for no reason, but when i tried to decrease string size with wasm binary it stopped happening

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,18 @@ import type * as esbuild from "esbuild"
 import fs from "fs"
 import { PLUGIN_NAME } from "./const"
 import path from "path"
+import assert from "assert"
 
-export const altvEsbuildRustWasm = (): esbuild.Plugin => {
+export interface Options {
+  /**
+   * Absolute path of .wasm file for `alt.File.read` (for example `'/client/rust_wasm_bg.wasm'`)
+   */
+  wasmPathForClientRead: string
+}
+
+export const altvEsbuildRustWasm = ({ wasmPathForClientRead }: Options): esbuild.Plugin => {
+  assert.ok(wasmPathForClientRead != null, "Expected wasmPathForClientRead option")
+
   return {
     name: PLUGIN_NAME,
     setup(build): void {
@@ -20,18 +30,18 @@ export const altvEsbuildRustWasm = (): esbuild.Plugin => {
       build.onLoad({ filter: /.*/, namespace }, ({ path: userPath, pluginData: resolveDir }) => {
         const wasmPath = path.resolve(resolveDir, userPath)
 
+        const outdir = build.initialOptions.outdir
+        assert.ok(outdir != null, "esbuild outdir option must present to copy .wasm to it")
+        const targetWasmPath = path.join(outdir, path.basename(wasmPath))
+        console.log("copying", wasmPath, "->", targetWasmPath)
+        fs.copyFileSync(wasmPath, targetWasmPath)
+
         const jsFilePath = path.resolve(
           path.dirname(wasmPath),
           path.basename(wasmPath, ".wasm")
             .slice(0, -3) + // removing _bg at the end
             ".js",
         )
-
-        const wasmBinary = fs.readFileSync(wasmPath)
-        const wasmBinaryString = wasmBinary.reduce((acc, value) => {
-          return acc + value.toString() + "|"
-        }, "")
-
         let jsFileContent = fs.readFileSync(jsFilePath).toString()
 
         jsFileContent = jsFileContent
@@ -40,17 +50,13 @@ export const altvEsbuildRustWasm = (): esbuild.Plugin => {
 
         return {
           contents: `
-            function decodeWasmBinaryString(bytes) {
-              bytes = bytes.split("|").slice(0, -1).map(Number)
-              return new Uint8Array(bytes).buffer
-            }
-
+            import alt from "alt-client";
             const wasmLoader = (altv_imports) => {
               // ------- jsFileContent
               ${jsFileContent}
               // ------- jsFileContent
 
-              const wasmArrayBuffer = decodeWasmBinaryString("${wasmBinaryString}");
+              const wasmArrayBuffer = alt.File.read("${wasmPathForClientRead}", "binary");
               initSync(wasmArrayBuffer);
 
               return wasm_bindgen;
